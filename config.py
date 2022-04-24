33d4a5ce7f038df2b493b9db516dc9b730f5b055
Malo <77781539+malorrx@users.noreply.github.com>
2022-04-24 23:17:43 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-API_KEY_BINANCE = 'ceoYkTNaXJspgbKH1qmr759foPX9XjGW2xxWRWV3X9GhpHGojSZ3tIt6t0oL4KoJ'
-API_SECRET_BINANCE = 'j194o4nTRJjxZhxuxwf1DOlnKJJCo70oTeJPFsGnFsZjtyvz4uFD5wRB6HdXkguO'
-
-host_db = "192.168.1.182"
-user_db =  "connector"
-port_db = 3306
-password_db = "azerty"
-database = "trading_bot"  
