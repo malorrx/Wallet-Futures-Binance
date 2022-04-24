bcae6ab988e8d22cbbbc5198be8057738c0676b2
malorrx <malo.riou@orange.fr>
2022-04-24 22:54:05 +0200

    init

---
+++ Wallet_connector.py
@@ -0,0 +1,43 @@
+from asyncio import futures
+from binance.client import Client
+from binance.enums import *
+import time
+import numpy as np
+import talib
+import config
+import mysql.connector
+
+#connexion à l'API de Binance
+client = Client(config.API_KEY_BINANCE, config.API_SECRET_BINANCE)
+
+def getUSDTbalance():
+    balance = client.futures_account_balance()
+    futures_account = client.futures_account()
+    totalWalletBalance = 0
+    availableBalance = 0
+    for actif in balance:
+        totalWalletBalance = totalWalletBalance + float(actif['balance'])
+        availableBalance = availableBalance + float(actif['withdrawAvailable'])
+    time.sleep(0.2)
+    return totalWalletBalance, futures_account['totalUnrealizedProfit'], availableBalance
+
+def addBalance(Balance, unrealizedPNL, availUSDT):
+    connection_params = {
+        'host' : config.host_db,
+        'user': config.user_db,
+        'password' : config.password_db,
+        'database' : config.database,
+    }
+    request = """insert into wallet
+             (Balance, Unrealized_PNL, availableUSDT)
+             values (%s, %s, %s)"""
+    params = (Balance, unrealizedPNL, availUSDT)
+    try:
+        with mysql.connector.connect(**connection_params) as db :
+            with db.cursor() as c:
+                c.execute(request, params)
+                db.commit()
+                return True
+    except Exception as err:
+        print('mysql_connector.addBallance Error : ' + str(err))
+        return False
---
+++ config.py
@@ -0,0 +1,7 @@
+API_KEY_BINANCE = 'ceoYkTNaXJspgbKH1qmr759foPX9XjGW2xxWRWV3X9GhpHGojSZ3tIt6t0oL4KoJ'
+API_SECRET_BINANCE = 'j194o4nTRJjxZhxuxwf1DOlnKJJCo70oTeJPFsGnFsZjtyvz4uFD5wRB6HdXkguO'
+
+host_db = "192.168.1.182"
+user_db =  "connector"
+password_db = "azerty"
+database = "trading_bot"
